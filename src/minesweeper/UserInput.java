2c4d3e1803b5aed0f80edf062451dd2120f34bf8
Hannah Kok <hannahkok93@outlook.com>
2023-05-08 20:04:38 +1000

    add user input class to separate user input from main class

---
+++ src/minesweeper/UserInput.java
@@ -0,0 +1,19 @@
+package minesweeper;
+
+public class UserInput {
+	int[] userInput;
+	
+	public UserInput(int[] userInput) {
+		this.userInput = userInput;
+	}
+
+	public int[] getUserInput() {
+		return userInput;
+	}
+
+	public void setUserInput(int[] userInput) {
+		this.userInput = userInput;
+	}
+	
+	
+}

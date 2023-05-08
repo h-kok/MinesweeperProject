ee8429e5984eb37f55dbe801156a21db8f6a37c1
Hannah Kok <hannahkok93@outlook.com>
2023-05-08 20:07:49 +1000

    update main class functions following inclusion of user input class, attempted to improve error handling for user input type mismatch or out of bounds values, but still not working as should

--- src/minesweeper/Main.java
+++ src/minesweeper/Main.java
@@ -5,11 +5,14 @@ import java.util.*;
 public class Main {
 	public static void main (String[] args) {
 		
-		int gridDimensions;
+		int gridDimensions = 0;
 		boolean notGameOver = true;
-		int x;
-		int y;
-		int[] input = new int[2];
+		int x = 0;
+		int y = 0;
+		int[] input = new int[] {x,y};
+
+		UserInput userInput = new UserInput(input);
+		
 		String count;
 		
 		System.out.println("Welcome to minesweeper.");
@@ -28,7 +31,7 @@ public class Main {
 			}
 		}
 
-		while (gridDimensions <=1) {
+		while (gridDimensions <=1) {		
 			System.out.println("The minimum possible dimension is 2x2. \nEnter a number to set the dimensions of the mine (min: 2): ");
 			gridDimensions = s.nextInt();
 		}
@@ -38,17 +41,13 @@ public class Main {
 		System.out.printf("2. There are %d hidden bombs in the grid. \nIf you select one of the bombs, game over. If you select a safe coordinate, the number revealed represents how many bombs are located in the immediate coordinates surrounding it.\n", gridDimensions);
 		System.out.println("\n3. Try to reveal all coordinates without setting the bombs off. Happy playing.\n");
 		
-		//make grid
-		Grid minesweeper = new Grid(gridDimensions);
-		ArrayList<ArrayList<String>> grid = minesweeper.createGrid();
-		minesweeper.printGrid(grid);
-		
-		//make bombs
+		Grid minesweeper = new Grid(gridDimensions, input);
+		minesweeper.printGrid();
+
 		Bomb bombs = new Bomb(gridDimensions);
-		ArrayList<ArrayList<Integer>> bombLocations = bombs.createBombLocations();
 		
-		while(bombs.hasDuplicates(bombLocations)==true) {
-			bombLocations = bombs.createBombLocations();
+		while(bombs.hasDuplicates()==true) {
+			bombs.setBombLocations();
 		}
 		
 		while (notGameOver) {
@@ -58,39 +57,86 @@ public class Main {
 					System.out.println("\nPlease enter a pair of coordinates, separated by a *space* and hit 'enter': ");
 					x = s.nextInt();
 					y = s.nextInt();
+					input[0] = x;
+					input[1] = y;
 					break;
 				}
 				catch (InputMismatchException e) {
+					System.out.printf("%d, %d", x, y);
 					System.out.println("One of your values was not a number, Please try again.");
 					s.next();
 					continue;
 				}
 			}
 			
+//			userInput.setUserInput(input); 
+//			System.out.println(Arrays.toString(input));
+			
+////			while (true ) {
+//				try {
+//					System.out.println("\nPlease enter a row number and hit 'enter': ");
+//					x = s.nextInt();
+////					y = s.nextInt();
+//					while (x < 1 || x > gridDimensions) {
+//						System.out.printf("Your value is out of range. \nColumn value should be from 1 to %d. \n please enter a row number:", gridDimensions);
+//						x = s.nextInt();
+//					}
+//					input[0] = x;
+////					input[1] = y;
+////					userInput.setUserInput(input);
+////					break;
+//				} catch (IndexOutOfBoundsException | InputMismatchException e) {
+//					System.out.printf("Your value is not a number. \nRow value should be from 1 to %d.", gridDimensions);
+////					s.next();
+////					continue;
+//				}
+//				
+////			}
+//			
+////			while ( true) {
+//				try {
+//					System.out.println("\nPlease enter a column number and hit 'enter': ");
+////					x = s.nextInt();
+//					y = s.nextInt();
+//					while ( y < 1 || y > gridDimensions) {
+//						System.out.printf("Your value is out of range or not a number. \nColumn value should be from 1 to %d. \nPlease enter a column number:", gridDimensions);
+//						y = s.nextInt();
+////						break;
+//					}
+////					input[0] = x;
+//					input[1] = y;
+//					
+////					break;
+//				} catch (IndexOutOfBoundsException | InputMismatchException e) {
+//					System.out.printf("Your value is out of range or not a number. \nColumn value should be from 1 to %d.", gridDimensions);
+////					s.next();
+////					continue;
+//				}
+//				
+////			}
+			
 			while (x < 1 || x > gridDimensions || y < 1 || y > gridDimensions) {
-				System.out.printf("One of your coordinates is out of range. \nCoordinate values should be from 1 to %d. \nPlease enter a pair of coordinates, separated by a *space* and hit 'enter': ", gridDimensions);
+				System.out.printf("Your value is out of range or not a number. \nColumn value should be from 1 to %d.", gridDimensions);
 				x = s.nextInt();
 				y = s.nextInt();
 			}
-			
 			input[0] = x;
 			input[1] = y;
-			
-			if (bombs.isBombLocation(bombLocations,input) == true) {
+			userInput.setUserInput(input);
+
+			if (bombs.isBombLocation(userInput.getUserInput()) == true) {
 				System.out.println("Boom! \nGame Over. Better luck next time.");
-				minesweeper.updateGrid(grid, input, "!");
-				minesweeper.updateGridWithBombs(grid, bombLocations);
-				minesweeper.printGrid(grid);
+				minesweeper.updateGrid("!");
+				minesweeper.updateGridWithBombs(bombs.getBombLocations());
+				minesweeper.printGrid();
 				notGameOver = false;
 			} else {
-				ArrayList<ArrayList<Integer>>surroundingCoords = minesweeper.getSurroundingCoordinates(input);
-				count = minesweeper.checkForSurroundingBombs(surroundingCoords, bombLocations);
-//				minesweeper.updateGrid(input, count);
-				minesweeper.updateGrid(grid, input, count);
-				minesweeper.printGrid(grid);
+				count = minesweeper.checkForSurroundingBombs(bombs.getBombLocations());
+				minesweeper.updateGrid(count);
+				minesweeper.printGrid();
 			}
 			
-			if (bombs.allBombsFound(grid) == true) {
+			if (minesweeper.allBombsFound(gridDimensions) == true) {
 				System.out.println("Congratulations, you found all the bombs!");
 				notGameOver = false;
 			}
